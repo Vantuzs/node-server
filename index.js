73884a4230636360e784ddeba438dd65f1b9ce5b
Vantuz <Shcherbina444@gmail.com>
2025-02-15 12:12:39 +0200

    Add yup validation

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const app = express()
+const yup = require('yup');
+const app = express();
+
+const bodyParser = express.json() // request.body
+
+const validationSchema = yup.object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+    isSubscribed: yup.boolean().required()
+})
 
 const PORT = 5000;
 
-app.get('/',(request,response)=>{
-    response.status(418).send('PRIVET')
-});
-
-app.get('/user',(request,response)=>{
-    response.status(418).send('????? TI V USERERERERERERE')
+app.post('/user',bodyParser,async (req,res,next)=>{
+    try{
+        const value = await validationSchema.validate(req.body);
+    console.log(value);
+    } catch(error){
+        next(error.message)
+    }
 })
 
 app.listen(PORT,()=>{
@@ -16,3 +28,15 @@ app.listen(PORT,()=>{
 })
 
 // маршрут + метод = роут
+
+/* 
+
+Задача: Зарегестрировать (создать) юзера 
+
+1. Принять запрос на определённый роут
+2. Разпарсить данные, которые пришли с запросом
+3. Проверить (валидировать) данные
+4. Сохранить эти данные
+5. Отправить ответы клиенту
+
+*/
