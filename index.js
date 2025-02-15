26767a5f651f40a6e3d46e55f13bd2ad8f5edb3f
Vantuz <Shcherbina444@gmail.com>
2025-02-15 11:11:45 +0200

    Express welcome

--- index.js
+++ index.js
@@ -1,61 +1,18 @@
-const http = require('http');
-const fs = require('fs/promises')
+const express = require('express');
+const app = express()
 
-const PORT = 5000
+const PORT = 5000;
 
-const requestListener = async (request,response)=>{
-    const {url,method} = request
+app.get('/',(request,response)=>{
+    response.status(418).send('PRIVET')
+});
 
-    // response.end('123')
-    
-   if(method==='GET'){
-    if(url === '/index.html'){// gttp://localhost:5000/index.html
-        try{
-            const data = await fs.readFile('./views/index.html','utf-8');
-        response.statusCode = 200;
-        response.end(data);
-    }
-        catch(error){
-            response.statusCode = 404;
-            response.end();
-        }
-    }else if(url === '/style.css'){
-        try{
-            const data = await fs.readFile('./views/style.css','utf-8')
-        response.statusCode = 200
-        response.end(data)
-    }catch(error){
-        response.statusCode = 200
-        response.end()
-    }
-    }else{
-        response.statusCode= 418;
-        response.end();
-    }
-   } else if(method==='POST'){
-        if(url === '/user'){
-            let jsonString = ''
-            request.on('data',chunk=>{
-                jsonString+= chunk;
-            });
-            request.on('end',()=>{
-                const user = JSON.parse(jsonString);
-                console.log(user);
-                response.statusCode = 200;
-                response.end('END )))))')
-            })
-        }
-   }
- 
+app.get('/user',(request,response)=>{
+    response.status(418).send('????? TI V USERERERERERERE')
+})
 
-    // Для закрытия сервера в ручную
-        // process.exit(0);
-  
-    
-}
+app.listen(PORT,()=>{
+    console.log(`Servet start on port ${PORT}`);
+})
 
-const server = http.createServer(requestListener);
-
-server.listen(PORT)
-
-// console.log(server);
+// маршрут + метод = роут
